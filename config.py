f9754d9e0d4ba576b057bc5ae970fbf9d6ab87ce
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-27 13:44:25 -0800

    added in requirements and PSQL config

--- config.py
+++ config.py
@@ -1,7 +1,29 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    # WTForms token
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # PSQL/SQL Alchemy Database Config
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost/dev_flask-blog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
