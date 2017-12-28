33cb7aff281d0251bf38751ea89ae5226889b46a
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-28 14:47:59 -0800

    imported modules for user management into routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 @app.route('/')
