84d9d0abb5b4c12d5ff7445fdbbaec92ae203566
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-28 18:57:13 -0800

    updating forms to use bootsrap markup and styles

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import loging
-from logging.handlers import SMTPHandler
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 # For Email
 if not app.debug:
@@ -29,5 +32,18 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    # Text logging
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
