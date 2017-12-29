c02225c0297caf78975c3fba1ca0dbdca10f392b
nilesmcgiver <zen.mind.pdx@gmail.com>
2017-12-28 18:31:40 -0800

    setup for email via smtp

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 A Flask Blog
+
+to start the application
+
+# python3 -m venv venv
+# virtualenv -p python3 venv
+# source venv/bin/activate
+# pip install -r requirements.txt
+# export FLASK_APP=flask-blog.py
+# run flask
+
+To turn on the smtp service needed for email alerts:
+“python -m smtpd -n -c DebuggingServer localhost:25"
--- config.py
+++ config.py
@@ -16,6 +16,14 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # Email Config settings
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+
 class ProductionConfig(Config):
     DEBUG = False
 
