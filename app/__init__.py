fdb1733ee47962b779dcf5728ee8e94e06b16b87
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-01-04 18:23:25 -0800

    added in email module

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,12 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+mail = Mail(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
